9fee4b52d7548700dc579636ab3335b964de9319
Cameron King <cking@ckxng.com>
2021-12-02 06:57:37 +0000

    chore: removes unnecessary code from test

--- advent_test.go
+++ advent_test.go
@@ -13,7 +13,5 @@ func TestFormatResults(t *testing.T) {
 	input := []Puzzle{{name: "A", result: "B"}, {name: "C", result: "D"}}
 	if got := FormatResults(input); got != want {
 		t.Errorf("FormatResults() = %q, want %q", got, want)
-	} else {
-		t.Logf("FormatResults() want ")
 	}
 }
